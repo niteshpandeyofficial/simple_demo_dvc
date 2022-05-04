a303207f677342473b2c5d19e9b5876114b869da
Nitesh Pandey <niteshpandeyofficial@gmail.com>
2022-05-04 13:20:46 +0530

    train_and_evaluate file created

---
+++ src/train_and_evaluate.py
@@ -0,0 +1,57 @@
+import os
+import argparse
+import pandas as pd
+import joblib
+import json
+import numpy as np
+from sklearn.linear_model import ElasticNet
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_error
+from urllib.parse import urlparse
+from get_data import read_params
+
+
+def eval_matrics(actual,prediction):
+    mse=mean_squared_error(actual,prediction)
+    rmse=np.sqrt(mse)
+    r2=r2_error(actual,prediction)
+    return (mse,rmse,r2)
+
+
+def train_and_evaluate(config_path):
+    config=read_params(config_path)
+    test_data_path=config["split_data"]["test_path"]
+    train_data_path=config["split_data"]["train_path"]
+    random_state=config["base"]["random_state"]
+    model_dir=config["model_dir"]
+
+    alpha=config["estimator"]["ElasticNet"]["params"]["alphas"]
+    l1_ratio=config["estimator"]["ElasticNet"]["params"]["l1_ratio"]
+
+    target=config["base"]["target_col"]
+
+    train=pd.read_csv(train_data_path,sep=",")
+    test=pd.read_csv(test_data_path,sep=",")
+
+    train_x=train.drop(target,axis=1)
+    test_x=test.drop(target,axis=1)
+
+    train_y=train[target]
+    test_y=test[target]
+
+    lr=ElasticNet(alpha=alpha,
+                l1_ratio=l1_ratio,
+                random_state=random_state)
+    lr.fit(train_x,train_y)
+    predicted_qualities=lr.predict(test_x)
+    (mse,rmse,r2)=eval_matrics(test_y,predicted_qualities)
+    print(f"value of mse:{mse} ,rmse:{rmse},r2:{r2}")
+
+
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args=args.parse_args()
+    train_and_evaluate(config_path=parsed_args.config)
