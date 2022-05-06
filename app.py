5863cb6268f7d903ebb9e3c59f5f82c2c2195351
Nitesh Pandey <niteshpandeyofficial@gmail.com>
2022-05-06 14:39:52 +0530

    Changes done in app and requirements file

--- app.py
+++ app.py
@@ -10,8 +10,15 @@ webapp_root="webapp"
 
 static_dir=os.path.join(webapp_root,"static")
 template_dir=os.path.join(webapp_root,"templates")
-
+print("File location",template_dir)
 app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
+
+@app.route("/",methods=['POST','GET'])
+def index():
+    if request.method=="POST":
+        pass
+    else:
+        render_template("index.html")
 
 if  __name__=="__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
