01b6100547d187f55f857b20c346b1ac836660af
Nitesh Pandey <niteshpandeyofficial@gmail.com>
2022-05-06 17:02:24 +0530

    details updated in app file

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import yaml
 import numpy as np
 
 
+
 params_path="params.yaml"
 webapp_root="webapp"
 
@@ -13,12 +14,56 @@ template_dir=os.path.join(webapp_root,"templates")
 print("File location",template_dir)
 app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
 
+
+def read_params(config_path):
+    with open(config_path) as ymal_file:
+        config=yaml.safe_load(ymal_file)
+    return config
+
+def predict(data):
+    config=read_params(params_path)
+    model_dir_path=config["webapp_model_dir"]
+    model=joblib.load(model_dir_path)
+    prediction=model.predict(data)
+    print(prediction)
+    return prediction[0]
+
+
+def api_response(request):
+    try:
+        data=np.array([list(request.json.values())])
+        response=predict(data)
+        response={"response":response}
+        return response
+    except Exception as e:
+        print(e)
+        error = {"error": "Something went wrong!! Try again later!"}
+        return error
+
+
+
 @app.route("/",methods=['POST','GET'])
 def index():
-    if request.method=="POST":
-        pass
+    if request.method == "POST":
+        try:
+            if request.form:
+                data = dict(request.form).values()
+                data = [list(map(float,data))]
+                response=predict(data)
+                return render_template("index.html", response=response)
+
+            elif request.json:
+                response = api_response(request)
+                return jsonify(response)
+
+        except Exception as e:
+            print(e)
+            error = {"error": "Something went wrong!! Try again later!"}
+            error = {"error": e}
+
+        return render_template("404.html", error=error)
     else:
-        render_template("index.html")
+        return render_template("index.html")
 
 if  __name__=="__main__":
     app.run(host="0.0.0.0",port=5000,debug=True)
