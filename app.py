94162220578d5561d010cd49b0f06b34bdbd1288
Nitesh Pandey <niteshpandeyofficial@gmail.com>
2022-05-08 15:27:02 +0530

    details added in app file

--- app.py
+++ app.py
@@ -4,42 +4,13 @@ import joblib
 import yaml 
 import numpy as np
 
-
-
-params_path="params.yaml"
 webapp_root="webapp"
+
 
 static_dir=os.path.join(webapp_root,"static")
 template_dir=os.path.join(webapp_root,"templates")
 print("File location",template_dir)
 app=Flask(__name__,static_folder=static_dir,template_folder=template_dir)
-
-
-def read_params(config_path):
-    with open(config_path) as ymal_file:
-        config=yaml.safe_load(ymal_file)
-    return config
-
-def predict(data):
-    config=read_params(params_path)
-    model_dir_path=config["webapp_model_dir"]
-    model=joblib.load(model_dir_path)
-    prediction=model.predict(data)
-    print(prediction)
-    return prediction[0]
-
-
-def api_response(request):
-    try:
-        data=np.array([list(request.json.values())])
-        response=predict(data)
-        response={"response":response}
-        return response
-    except Exception as e:
-        print(e)
-        error = {"error": "Something went wrong!! Try again later!"}
-        return error
-
 
 
 @app.route("/",methods=['POST','GET'])
